82a4e3a8280e7b20cfea72b45436454a9d7982fa
Albert <albertirura11@gmail.com>
2023-12-12 18:51:56 +0300

    basic asyncio

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""basic asyncio syntax"""
+"""async coroutine"""
 import asyncio
 import random
 
-
 async def wait_random(max_delay: int = 10) -> float:
-    """implement random delay and return it"""
-    duration = random.uniform(0, max_delay)
-    await asyncio.sleep(duration)
-    return duration
+    """async coroutine"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""concurrent execution"""
+import asyncio
+from typing import List
+from heapq import nsmallest
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """concurrent execution"""
+    delays = []
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
+    return nsmallest(n, delays)
